8e6eb621903ff7869c7fb3408539c7b80859e4e8
dmartana <daniel.martana@greenwavereality.com>
2017-06-22 14:10:04 +0800

    remove redundant parenthesis in if-clauses

--- problem167/twoSumII.go
+++ problem167/twoSumII.go
@@ -10,16 +10,16 @@ func twoSum1(numbers []int, target int) []int { // this is slow
 		dist := 1
 
 		for {
-			if (i + dist >= len(numbers)) {
+			if i + dist >= len(numbers) {
 				break
 			}
 			first := numbers[i]
 			second := numbers[i+dist]
 			fmt.Println([]int{first, second})
-			if (second + first > target) {
+			if second + first > target {
 				break;
 			}
-			if (second + first == target) {
+			if second + first == target {
 				return []int{i+1, i+dist+1}
 			}
 			dist++
@@ -32,16 +32,16 @@ func twoSum(numbers []int, target int) []int { // this is fast
 	fidx := 0
 	bidx := len(numbers) - 1
 	for {
-		if (numbers[fidx] + numbers[bidx] > target) {
+		if numbers[fidx] + numbers[bidx] > target {
 			bidx--
 		}
-		if (numbers[fidx] + numbers[bidx] < target) {
+		if numbers[fidx] + numbers[bidx] < target {
 			fidx++
 		}
-		if (numbers[fidx] + numbers[bidx] == target) {
+		if numbers[fidx] + numbers[bidx] == target {
 			return []int{fidx+1, bidx+1}
 		}
-		if (fidx >= bidx) {
+		if fidx >= bidx {
 			break
 		}
 	}
