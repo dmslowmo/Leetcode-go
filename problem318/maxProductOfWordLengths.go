a5073a2c905410ea99d6ae3baaf7e3cb094837ef
dmslowmo <daniel.martana@gmail.com>
2017-06-29 20:55:51 +0800

    Problem 318: Max products of word lengths

--- problem318/maxProductOfWordLengths.go
+++ problem318/maxProductOfWordLengths.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 	"fmt"
+	"sort"
 )
 
 func hasNoOverlapChar(word1 string, word2 string) bool {
@@ -16,31 +17,50 @@ func hasNoOverlapChar(word1 string, word2 string) bool {
 	return !strings.ContainsAny(word1, word2)
 }
 
-func maxProduct(words []string) int {
-	wordMap := make(map[string][]string)
+// A data structure to hold key/value pairs
+type Pair struct {
+	Key   string
+	Value int
+}
 
-	for i:=0;i<len(words);i++ {
-		for j := 0; j < len(words); j++ {
+// A slice of pairs that implements sort.Interface to sort by values
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func maxProduct(words []string) int {
+	sortedByLength := make(map[string]int)
+	for _, word := range words {
+		sortedByLength[word] = len(word)
+	}
+	// sort the sequence based on values ie occurrences
+	p := make(PairList, len(sortedByLength))
+
+	i := 0
+	for k, v := range sortedByLength {
+		p[i] = Pair{k, v}
+		i++
+	}
+
+	sort.Sort(sort.Reverse(p)) // sorted from longest to shortest word
+
+	max := 0
+	limit := len(p) - 1
+	for i:=0;i<=limit;i++ {
+		for j := 0; j <= limit; j++ {
 			if i == j {
 				continue
 			}
-			if hasNoOverlapChar(words[i], words[j]) {
-				if wordlist, ok := wordMap[words[i]]; ok {
-					wordMap[words[i]] = append(wordlist, words[j])
-				} else {
-					wordMap[words[i]] = []string{words[j]}
+			if len(p[i].Key) * len(p[j].Key) > max {
+				if hasNoOverlapChar(p[i].Key, p[j].Key) {
+					max = len(p[i].Key) * len(p[j].Key)
+					limit = j
+					break
 				}
 			}
-		}
-	}
 
-	max := 0
-	for key, wordlist := range wordMap {
-		for _, word := range wordlist {
-			product := len(key) * len(word)
-			if product > max {
-				max = product
-			}
 		}
 	}
 
